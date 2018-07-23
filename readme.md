feb5a43db5394cdf9ee9b5376bb9e23ed7d68993
Torey Hickman <torey@toreyhickman.com>
2018-07-23 11:33:21 -0400

    Add scheduled actionslambda example

--- readme.md
+++ readme.md
@@ -10,3 +10,5 @@ Examples for using the AWS SDK for Ruby to invoke project-specific lambda functi
 | AWS_SECRET_ACCESS_KEY           | AWS IAM user secret access key *required*
 | WORKPLACE_USER_ID               | Default person to receive messages
 | SEND_MESSAGE_TO_FACEBOOK_LAMBDA | Name of AWS lambda that sends chat messages to workplace users
+| SCHEDULED_ACTIONS_LAMBDA        | Name of AWS lambda that schedules actions (e.g., send message to facebook)
+| WATSON_WORKSPACE_NAME           | Name of the Watson workspace
--- runner.rb
+++ runner.rb
@@ -6,7 +6,7 @@ require 'pp'
 
 CLIENT = Aws::Lambda::Client.new
 
-def send_message_to_facebook(message = "Hello.", user_id = ENV["WORKPLACE_USER_ID"])
+def send_message_to_facebook(message: "Hello.", user_id: ENV["WORKPLACE_USER_ID"])
   CLIENT.invoke({
     client_context: Base64.encode64({}.to_json),
     invocation_type: "RequestResponse",
@@ -19,7 +19,7 @@ def send_message_to_facebook(message = "Hello.", user_id = ENV["WORKPLACE_USER_I
         facebook: {
           user: {
             id: user_id,
-            conversation: "workplace_operations"
+            conversation: ENV["WATSON_WORKSPACE_NAME"]
           }
         }
       },
@@ -48,4 +48,81 @@ def send_message_to_facebook(message = "Hello.", user_id = ENV["WORKPLACE_USER_I
   })
 end
 
-send_message_to_facebook
+def schedule_message(time: (Time.now.utc + 120), message: "Hello.", user_id: ENV["WORKPLACE_USER_ID"])
+  CLIENT.invoke({
+    client_context: Base64.encode64({}.to_json),
+    invocation_type: "RequestResponse",
+    function_name: ENV["SCHEDULED_ACTIONS_LAMBDA"],
+    payload: {
+      inputText: "",
+      intents: [],
+      requestContext: {},
+      responseContext: {
+        facebook: {
+          user: {
+            id: user_id,
+            conversation: ENV["WATSON_WORKSPACE_NAME"]
+          }
+        }
+      },
+      entities: [],
+      outputText: [],
+      session: {
+        conversation: "Outside of Workplace/Watson",
+        user: "Some process"
+      },
+      actionParameters:[
+        {
+          "name": "action",
+          "value": "save"
+        },
+        {
+          "name": "cronExpression",
+          "value": to_cron_expression(time)
+        },
+        {
+          "name": "actions",
+          "value": [
+            {
+              "function": "Skill-SendMessageToFacebook",
+              "parameters": [
+                {
+                  "name": "message",
+                  "value": message
+                },
+                {
+                  name: "message-type",
+                  value: "quick-reply"
+                },
+                {
+                  name: "replies",
+                  value: [
+                    {
+                      title: "whitelabel",
+                      payload: "whitelabel"
+                    }
+                  ]
+                }
+              ],
+              "responseContext": {
+                "facebook": {
+                  "user": {
+                    "id": user_id,
+                    "conversation": ENV["WATSON_WORKSPACE_NAME"]
+                  }
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }.to_json
+  })
+end
+
+def to_cron_expression(time)
+  time.strftime("0 %-M %k %e %-m * %Y")
+end
+
+send_message_to_facebook(message: "Sending this now.")
+schedule_message(message: "Scheduling this to be sent later.")
